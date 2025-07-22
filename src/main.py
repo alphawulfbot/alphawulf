f5589005e2d2297aeaae6577f0dec96d9e1b333f
Alpha Wulf <earn.alphawulf@gmail.com>
2025-07-22 13:30:03 +0530

    Updated main file

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request, jsonify
 from flask_cors import CORS
 import os
+import time # Added for health check timestamp
+
 from src.routes.user import user_bp
 from src.routes.admin import admin_bp
 from src.routes.upgrades import upgrades_bp
@@ -8,11 +10,14 @@ from src.routes.withdrawal import withdraw_bp
 from src.routes.referrals import referral_bp
 from src.routes.minigames import minigames_bp
 
-app = Flask(__name__, static_folder='static', template_folder='templates')
-app.secret_key = os.environ.get('SECRET_KEY', 'alphawulf2025secretkey')
+# Import the new auth blueprint
+from src.routes.auth import auth_bp
 
-# Enable CORS
-CORS(app, resources={r"/api/*": {"origins": "*"}})
+app = Flask(__name__, static_folder=\'static\', template_folder=\'templates\')
+app.secret_key = os.environ.get(\'SECRET_KEY\', \'alphawulf2025secretkey\')
+
+# Enable CORS for all routes
+CORS(app, resources={r\"/api/*\": {\"origins\": \"*\"}}, allow_headers=[\"Content-Type\", \"Accept\"], methods=[\"GET\", \"POST\", \"PUT\", \"DELETE\", \"OPTIONS\"])
 
 # Register blueprints
 app.register_blueprint(user_bp)
@@ -22,14 +27,18 @@ app.register_blueprint(withdraw_bp)
 app.register_blueprint(referral_bp)
 app.register_blueprint(minigames_bp)
 
-@app.route('/')
-def index():
-    return send_from_directory(app.static_folder, 'index.html')
+# Register the new auth blueprint
+app.register_blueprint(auth_bp)
 
-@app.route('/<path:path>')
+@app.route(\'/\')
+def index():
+    return send_from_directory(app.static_folder, \'index.html\')
+
+@app.route(\'/<path:path>\')
 def static_files(path):
     return send_from_directory(app.static_folder, path)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+if __name__ == \'__main__\':
+    app.run(host=\'0.0.0.0\', port=int(os.environ.get(\'PORT\', 5000)), debug=True)
+
 
